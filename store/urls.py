d0482b26f7fb0263ffb9c5e56eaf336f41ecbcb9
ManyuDhyani <manyudhyani@gmail.com>
2022-05-02 19:52:45 +0530

    first commit

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from store import views
+urlpatterns = [
+    path('', views.home),
+    path('product-detail', views.product_detail, name='product-detail'),
+    path('cart', views.add_to_cart, name='add-to-cart'),
+    path('buy', views.buy_now, name='buy-now'),
+    path('profile', views.profile, name='profile'),
+    path('address', views.address, name='address'),
+    path('orders', views.orders, name='orders'),
+    path('changepassword', views.change_password, name='changepassword'),
+    path('mobile', views.mobile, name='mobile'),
+]
