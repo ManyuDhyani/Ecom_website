3ec66f5603d2751feeb93e6b109c5d6061f8dd71
ManyuDhyani <manyudhyani@gmail.com>
2022-05-03 20:12:20 +0530

    Models for Product, Cart and Order Placed.

--- store/models.py
+++ store/models.py
@@ -19,3 +19,44 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
+    ('BW', 'Bottom Wear'),
+)
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='products')
+
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+STATUS_CHOICES = (
+    ('Accepted', 'Accepted'),
+    ('Shipped', 'Shipped'),
+    ('Coming Tomorrow', 'Coming Tomorrow'),
+    ('Delivered', 'Delivered'),
+    ('Cancelled', 'Cancelled')
+)
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
