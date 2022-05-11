9dec2c228f5541eb9c9014a33f811c16dcb2bad4
ManyuDhyani <manyudhyani@gmail.com>
2022-05-11 21:09:03 +0530

    Ajax and js functionality for adding quantity in cart and backend integration with DB for the same.

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('product-detail/<int:pk>/', views.product_detail.as_view(), name='product-detail'),
     path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
     path('cart/', views.show_cart, name='show_cart'),
+
+    path('pluscart/', views.plus_cart),
+
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views import View
 from .models import *
@@ -39,6 +41,29 @@ def show_cart(request):
    return render(request, 'app/addtocart.html', {'carts': carts, 'total_amount': total_amount, 'amount': amount})
   else:
    return  render(request, 'app/emptycart.html')
+
+def plus_cart(request):
+ if request.method == 'GET':
+  prod_id = request.GET['prod_id']
+  c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+  c.quantity += 1
+  c.save()
+  amount = 0.0
+  shipping_amount = 70.0
+  total_amount = 0.0
+  cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+
+  for p in cart_product:
+   tempamount = (p.quantity * p.product.discounted_price)
+   amount += tempamount
+   total_amount = amount + shipping_amount
+
+  data = {
+   'quantity': c.quantity,
+   'amount': amount,
+   'total_amount': total_amount,
+  }
+  return  JsonResponse(data)
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
